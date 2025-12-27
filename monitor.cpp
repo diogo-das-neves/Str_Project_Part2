c2ea6058de49ff0cebab7fae92c59a5db616531c
Yigantopitekus <157696237+Yigantopitekus@users.noreply.github.com>
2025-12-27 19:04:44 +0100

    Implement date and time commands in monitor
    Added new commands for reading and setting date and time.

--- monitor.cpp
+++ monitor.cpp
@@ -11,9 +11,14 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include "mbed.h"
+#include "FreeRTOS.h"
+#include "semphr.h"
 
 extern char* my_fgets(char *, int, FILE *);
-
+extern SemaphoreHandle_t ClockMutex;
+extern Record maxtemp;
+extern Record mintemp;
 /*-------------------------------------------------------------------------+
 | Headers of command functions
 +--------------------------------------------------------------------------*/ 
@@ -21,6 +26,12 @@ extern void cmd_sair (int, char** );
 extern void cmd_test (int, char** );
        void cmd_sos  (int, char** );
 extern void cmd_send (int, char** );
+extern void cmd_rdt (int,char**);
+extern void cmd_sd (int,char**);
+extern void cmd_rc (int,char**);
+extern void cmd_rd (int,char**);
+extern void cmd_sc (int,char**);
+extern void cmd_rmm (int,char**);
 
 /*-------------------------------------------------------------------------+
 | Variable and constants definition
@@ -37,8 +48,12 @@ struct  command_d {
   {cmd_send, "send","<msg>            send message"},
   {cmd_sair, "sair","                 sair"},
   {cmd_test, "test","<arg1> <arg2>    test command_2"},
-  {},
-  {}
+  {cmd_rdt, "rdt" ,"                  read date and time"},          
+  {cmd_sd,"sd","<dd> <MM> <YY>        set date(dd:MM:YY)"},
+  {cmd_rd,"sd","                      read time"}
+  {cmd_sc,"sd","<hh> <mm> <ss>        set time(hh:mm:ss)"},
+
+
 };
 
 #define NCOMMANDS  (sizeof(commands)/sizeof(struct command_d))
@@ -107,4 +122,73 @@ void monitor (void)
   } /* forever */
 }
 
+void cmd_rdt(int argc, char** argv){
+    if(xSemaphoreTake(ClockMutex, 100)) {
+
+        tm tm;
+        time_t t;
+        time(&t);
+        localtime_r(&t, &tm);
+        printf("%s\n", ctime(&t));
+        xSemaphoreGive(ClockMutex);
+    } else {
+        printf("Failed to acquire ClockMutex\n");
+    }
+}   
+void cmd_sd(int argc, char** argv){
+    struct tm t;
+    time_t seconds;
+
+    if(xSemaphoreTake(ClockMutex, 100)) {
+        struct tm t;
+        time_t seconds;
+
+        time(&seconds);
+        localtime_r(&seconds, &t);
+
+        t.tm_mday = atoi(argv[1]);
+        t.tm_mon  = atoi(argv[2]) - 1;
+        t.tm_year = atoi(argv[3]) - 1900;
+
+        seconds = mktime(&t);
+        set_time(seconds);
+
+        xSemaphoreGive(ClockMutex);
+    } else {
+        printf("Failed to acquire ClockMutex\n");
+    }
+}
+
+void cmd_rc(int argc,char**argcv){
+    tm tm;
+    time_t t;
+    if(xSemaphoreTake(ClockMutex, 100)) {
+        time(&t);
+        localtime_r(&t, &tm);
+        printf("%d:%d:%d\n",tm.tm_hour,tm.tm_min,tm.tm_sec);
+        xSemaphoreGive(ClockMutex);
+    } else {
+        printf("Failed to acquire ClockMutex\n");
+    }
+
+}
+void cmd_sc(int argc, char** argv){
+    struct tm t;
+    time_t seconds;
+    if(xSemaphoreTake(ClockMutex, 100)) {
+
+        time(&seconds);
+        localtime_r(&seconds, &t);
+
+        t.tm_hour = atoi(argv[1]);
+        t.tm_min  = atoi(argv[2]) - 1;
+        t.tm_sec = atoi(argv[3]) - 1900;
+
+        seconds = mktime(&t);
+        set_time(seconds);
+        xSemaphoreGive(ClockMutex);
+    } else {
+        printf("Failed to acquire ClockMutex\n");
+    }
+}
 //#endif //notdef
