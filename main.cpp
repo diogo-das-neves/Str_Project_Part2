cd9a8c195838bb3ca2d4309845973dacc24c1ffc
Yigantopitekus <157696237+Yigantopitekus@users.noreply.github.com>
2026-01-08 00:52:24 +0000

    Refactor main.cpp for improved clarity and structure
    Refactor task and variable names for clarity and consistency. Introduce new mutexes and timers for better synchronization and timing control.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "queue.h"
 #include "semphr.h"
 #include "extras.h"
+#include "timers.h"
 
 #include "LM75B.h"
 #include "C12832.h"
@@ -13,18 +14,28 @@
 #include "MMA7660.h"
 
 
-volatile int TL = 10;
-volatile int TM = 25;
-volatile int PMON = 5;
-volatile int TALA = 10;
-
+volatile int low_threshold_TL = 10;
+volatile int high_threshold_TH = 25;
+volatile int monitoring_period_PMON = 5;
+volatile int alarm_duration_TALA = 10;
+volatile bool alarm_clock = 1;
+volatile bool temp_alarm = 1;
 SemaphoreHandle_t AlarmMutex;
 SemaphoreHandle_t ClockMutex;
 SemaphoreHandle_t RecordMutex;
 SemaphoreHandle_t ParamMutex;
+SemaphoreHandle_t StateMutex;
 
 TaskHandle_t xTask_temp;
-TaskHandle_t xTask_Alarm;
+TaskHandle_t xTask_AlarmClock;
+TaskHandle_t xTask_AlarmTemp;
+TaskHandle_t xTask_Bubble;
+TaskHandle_t xTask_Pot1;
+TaskHandle_t xTask_Pot2;
+
+
+
+TimerHandle_t SensorTimer;
 
 AnalogIn pot1(p19);
 AnalogIn pot2(p20);
@@ -44,11 +55,13 @@ PwmOut spkr(p26); //buzzer
 
 
 QueueHandle_t xQueue;
-QueueHandle_t xQueue2;
+
+SemaphoreHandle_t xMeasureTempSemaphore;
+SemaphoreHandle_t TempMutex;
+volatile float temperature; 
 
 extern void monitor(void); //shared vars have to be protected
 extern float sensor_read;
-volatile bool alarm;
 volatile float Period;
 volatile float DutyCycle;
 Record maxtemp;
@@ -73,180 +86,167 @@ char* my_fgets (char* ln, int sz, FILE* f)
   return ln;
 }
 
-void vTask1( void *pvParameters ) {
-int32_t lValueToSend;
-BaseType_t xStatus;
-    led1 = 1;
-    for( ;; ) {
-        lValueToSend = 201;
-        xStatus = xQueueSend( xQueue, &lValueToSend, 0 );
-        monitor(); //does not return
-        led1 = !led1;
-    }
+void vTask_Serial( void *pvParameters ) {
+    monitor(); //does not return
 }
 
-void vTask2( void *pvParameters ) {
-int32_t lReceivedValue;
-BaseType_t xStatus;
-
-    led2 = 1;
-    printf("Hello from mbed -- FreeRTOS / cmd\n");
-    for( ;; ) {
-//        vTaskDelay( 1000 );
-        xStatus = xQueueReceive( xQueue, &lReceivedValue, 1000 );
-        if( xStatus == pdPASS ) {
-            printf( "Received = %d", lReceivedValue );
-        }
-        led2 = !led2;
-    }
-}
-
-void vTask_MCU(void *pvParameters){
-    BaseType_t xStatus;
+void vTask_BubbleLevel(void *pvParameters){
     float x = 0;
     float y = 0;
     for(;;){
         x = (x + MMA.x() * 16.0)/2.0;
         y = (y -(MMA.y() * 16.0))/2.0;
-        lcd.fillcircle(x+63, y+15, 3, 1); //draw bubble
-        lcd.circle(63, 15, 8, 1);
+        lcd.fillcircle(x+111, y+15, 3, 1); //draw bubble
+        lcd.circle(111, 15, 8, 1);
+        lcd.line(95,0,95,31,1); // draw margin line
         vTaskDelay(pdMS_TO_TICKS(100));
-        lcd.fillcircle(x+63, y+15, 3, 0); //erase bubble
+        lcd.fillcircle(x+111, y+15, 3, 0); //erase bubble
     }
 }
-void vTask_Alarm(void *pvParameters){
-    BaseType_t xStatus;
+void vTask_AlarmTemp(void *pvParameters){
 
     for(;;){
         ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-        if(xSemaphoreTake(AlarmMutex, 200)){
-            if(alarm){
-                spkr.period(Period);
-                spkr = DutyCycle;
-            } else {
-                spkr = 0.0f;
-            }
-            xSemaphoreGive(AlarmMutex);
-        }
-    vTaskDelay(pdMS_TO_TICKS(100));
+        MUTEX_TAKE(AlarmMutex)
+        spkr.period(Period);
+        spkr = DutyCycle;
+        MUTEX_RETURN(AlarmMutex)
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+}
+void vTask_AlarmClock(void *pvParameters){
+
+    for(;;){
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+        MUTEX_TAKE(AlarmMutex)
+        spkr.period(Period);
+        spkr = DutyCycle;
+        MUTEX_RETURN(AlarmMutex)
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
 void vTask_Pot1(void *pvParameters){
-    BaseType_t xStatus;
     float f;
     for(;;){
         f = pot1.read()*5000;
         if(f <= 0){f = 0.01;}
-        if(xSemaphoreTake(AlarmMutex, 100))
-            Period = 1/f;
-            xSemaphoreGive(AlarmMutex);
+        MUTEX_TAKE(AlarmMutex)
+        Period = 1/f;
+        MUTEX_RETURN(AlarmMutex)
         vTaskDelay(pdMS_TO_TICKS(200));
 
-        }
     }
+}
+
 void vTask_Pot2(void *pvParameters){
-    BaseType_t xStatus;
     for(;;){
-        if(xSemaphoreTake(AlarmMutex, 200))
-            DutyCycle = pot2.read();
-            xSemaphoreGive(AlarmMutex);
+        MUTEX_TAKE(AlarmMutex)
+        DutyCycle = pot2.read();
+        MUTEX_RETURN(AlarmMutex)
         vTaskDelay(pdMS_TO_TICKS(200));
-
-        }
     }
+}
     
 void vTask_temp(void *pvParameters){
-    BaseType_t xStatus;
-    float sensor_read;
     sensor.open();
     for(;;){
-        if(PMON > 0){
-            sensor_read = sensor.temp();
-            xStatus = xQueueSend(xQueue2, &sensor_read, 0);
-            vTaskDelay(pdMS_TO_TICKS(PMON*1000));//5 secs
-        }else {
-            ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-            sensor_read = sensor.temp();
-            xStatus = xQueueSend(xQueue2, &sensor_read, 0);
-            }
-
+        if(xSemaphoreTake(xMeasureTempSemaphore, portMAX_DELAY)) {
+            MUTEX_TAKE(TempMutex)
+            temperature = sensor.temp();
+            MUTEX_RETURN(TempMutex)
+        }
     }
 }
+static void TempTimerCallback(TimerHandle_t xTimer) {
+    xSemaphoreGive(xMeasureTempSemaphore);
+}
+
 
 void vTask_LCD(void *pvParameters){
-    BaseType_t xStatus;
-    float sensor_read;
     time_t t;
     tm tm;
+    float sensor_read;
     for(;;){
-        xStatus = xQueueReceive(xQueue2, &sensor_read, 1000);
-        if(xStatus==pdPASS){
-            if(xSemaphoreTake(ClockMutex,100))
-                time(&t);
-                localtime_r(&t, &tm);
-                lcd.locate(0,0); //3
-                lcd.printf("%d:%d:%d",tm.tm_hour,tm.tm_min,tm.tm_sec);
-                xSemaphoreGive(ClockMutex);
-            lcd.locate(0,11); //13
-            lcd.printf("A: C T");
-            lcd.locate(0,22); //26
-            lcd.printf("T(C) = %.3f\n", sensor_read);
-        }
+        MUTEX_TAKE(ClockMutex)
+        time(&t);
+        MUTEX_RETURN(ClockMutex)
+
+        MUTEX_TAKE(TempMutex)
+        sensor_read = temperature; // cache temperature
+        MUTEX_RETURN(TempMutex)
+
+        localtime_r(&t, &tm);
+        lcd.locate(0,0); //3
+        //lcd.fillrect(0,0,94,32,0); // clear framebuffer
+        lcd.printf("%02d:%02d:%02d",tm.tm_hour,tm.tm_min,tm.tm_sec);
+        lcd.locate(0,11); //13
+        lcd.printf("A: C T");
+        lcd.locate(0,22); //26
+
+        lcd.printf("T(C) =%7.3f\n", sensor_read);
+
+        vTaskDelay(pdMS_TO_TICKS(33)); //30Hz LCD
     }
 }
+
 void vTask_records(void *pvParameters){
-    BaseType_t xStatus;
     float sensor_read;
     time_t t;
     tm tm;
     for(;;){
-        xStatus = xQueueReceive(xQueue2, &sensor_read, 1000);
-        if(xStatus==pdPASS){
-            if(sensor_read > maxtemp.temp){
-                if(xSemaphoreTake(ClockMutex,200)){
-                    time(&t);
-                    localtime_r(&t, &tm);
-                    maxtemp.temp = sensor_read;
-                    maxtemp.timestamp.tm_sec = tm.tm_sec;
-                    maxtemp.timestamp.tm_min = tm.tm_min;
-                    maxtemp.timestamp.tm_hour = tm.tm_hour;
-                    xSemaphoreGive(ClockMutex);
-                    }
-            }
-            if(sensor_read < mintemp.temp){
-                if(xSemaphoreTake(ClockMutex,200)){
-                    time(&t);
-                    localtime_r(&t, &tm);
-                    mintemp.temp = sensor_read;
-                    mintemp.timestamp.tm_sec = tm.tm_sec;
-                    mintemp.timestamp.tm_min = tm.tm_min;
-                    mintemp.timestamp.tm_hour = tm.tm_hour;
-                    xSemaphoreGive(ClockMutex);
-                    }
-            }
-            
+        MUTEX_TAKE(TempMutex)
+        sensor_read = temperature; // cache temperature
+        MUTEX_RETURN(TempMutex)
+
+        MUTEX_TAKE(ClockMutex)
+        time(&t);
+        MUTEX_RETURN(ClockMutex)
+        localtime_r(&t, &tm);
+
+        if(sensor_read > maxtemp.temp){
+            maxtemp.temp = sensor_read;
+            maxtemp.timestamp.tm_sec = tm.tm_sec;
+            maxtemp.timestamp.tm_min = tm.tm_min;
+            maxtemp.timestamp.tm_hour = tm.tm_hour;
+            maxtemp.timestamp.tm_mday = tm.tm_mday;
+            maxtemp.timestamp.tm_mon = tm.tm_mon;
+            maxtemp.timestamp.tm_year = tm.tm_year;
         }
+        if(sensor_read < mintemp.temp){
+            mintemp.temp = sensor_read;
+            mintemp.timestamp.tm_sec = tm.tm_sec;
+            mintemp.timestamp.tm_min = tm.tm_min;
+            mintemp.timestamp.tm_hour = tm.tm_hour;
+            mintemp.timestamp.tm_mday = tm.tm_mday;
+            mintemp.timestamp.tm_mon = tm.tm_mon;
+            mintemp.timestamp.tm_year = tm.tm_year;
+        }
+        vTaskDelay(pdMS_TO_TICKS(500));
+        //this catches even the fastest periodic monitoring
     }
 }
+
 void vTask_Temp_Light_Alarm(void *pvParamaters){
-    BaseType_t xStatus;
     float sensor_read;
     for(;;){
-        xStatus = xQueueReceive(xQueue2, &sensor_read, 1000);
+        MUTEX_TAKE(TempMutex)
+        sensor_read = temperature; // cache temperature
+        MUTEX_RETURN(TempMutex)
+
         if (sensor_read >= 25){
             r = 0.8;
             g = 1;
             b = 1;
-            if(alarm && xSemaphoreTake(AlarmMutex, 500))
-                xTaskNotify(xTask_Alarm, 0,eNoAction);
+            if(xSemaphoreTake(AlarmMutex, 500))
+                xTaskNotify(xTask_AlarmTemp, 0,eNoAction);
             xSemaphoreGive(AlarmMutex);
         }
         else if(sensor_read <= 23){
             r = 1;
             g = 1;
             b = 0.8;
-            if(alarm && xSemaphoreTake(AlarmMutex, 500))
-                xTaskNotify(xTask_Alarm, 0,eNoAction);
+            if(xSemaphoreTake(AlarmMutex, 500))
+                xTaskNotify(xTask_AlarmTemp, 0,eNoAction);
             xSemaphoreGive(AlarmMutex);
         }
         else{
@@ -254,12 +254,16 @@ void vTask_Temp_Light_Alarm(void *pvParamaters){
             g = 0.8;
             b = 1;
         }
+
+        vTaskDelay(pdMS_TO_TICKS(33)); // 30 Hz
     }
 }
+
 void alarmFunction(void){
-    if(alarm)
-        xTaskNotify(xTask_Alarm, 0,eNoAction);
+    xTaskNotify(xTask_AlarmClock, 0,eNoAction);
 }
+
+
 int main( void ) {
     /* Perform any hardware setup necessary. */
 //    prvSetupHardware();
@@ -268,24 +272,46 @@ int main( void ) {
     set_time(0);
     pc.baud(115200);
 
+    r.period(0.00005); // some people (including me) get headaches from PWM frequencies
+    g.period(0.00005); // under a few kHz. Setting to 20kHz. Kat
+    b.period(0.00005);
+
     AlarmMutex = xSemaphoreCreateMutex();
     ClockMutex = xSemaphoreCreateMutex();
+    RecordMutex = xSemaphoreCreateMutex();
+    StateMutex = xSemaphoreCreateMutex();
+    ParamMutex = xSemaphoreCreateMutex();
+    TempMutex = xSemaphoreCreateMutex();
 //    printf("Hello from mbed -- FreeRTOS / cmd\n");
 
     /* --- APPLICATION TASKS CAN BE CREATED HERE --- */
 
     xQueue = xQueueCreate( 4, sizeof( int32_t ) );
-    xQueue2 = xQueueCreate( 4, sizeof( float ) );
+    vSemaphoreCreateBinary(xMeasureTempSemaphore);
+    xSemaphoreGive(xMeasureTempSemaphore);
 
-    //xTaskCreate( vTask1, "Task 1", 2*configMINIMAL_STACK_SIZE, NULL, 1, NULL );
-    xTaskCreate( vTask_Alarm, "Alarm Task", 2*configMINIMAL_STACK_SIZE, NULL, 2, &xTask_Alarm );
-    xTaskCreate( vTask_temp, "Temp Task", 2*configMINIMAL_STACK_SIZE, NULL, 1, &xTask_temp );
+
+    SensorTimer = xTimerCreate(
+        "SensorTimer",
+         pdMS_TO_TICKS(1000 * monitoring_period_PMON),
+         pdTRUE,
+         NULL,
+         TempTimerCallback);
+
+    xTaskCreate( vTask_Serial, "SerialComms Task", 2*configMINIMAL_STACK_SIZE, NULL, 1, NULL );
+    xTaskCreate( vTask_AlarmClock, "Alarm  clock Task", 2*configMINIMAL_STACK_SIZE, NULL, 2, &xTask_AlarmClock );
+    xTaskCreate( vTask_AlarmTemp, "Alarm temp Task", 2*configMINIMAL_STACK_SIZE, NULL, 2, &xTask_AlarmTemp );
+    xTaskCreate( vTask_temp, "Temp Task", 2*configMINIMAL_STACK_SIZE, NULL, 5, &xTask_temp );
     xTaskCreate( vTask_LCD, "LCD Task", 2*configMINIMAL_STACK_SIZE, NULL, 2, NULL );
     xTaskCreate( vTask_Temp_Light_Alarm, "TempAlarm Task", 2*configMINIMAL_STACK_SIZE, NULL, 2, NULL );
     xTaskCreate( vTask_records, "TempRecords Task", 2*configMINIMAL_STACK_SIZE, NULL, 2, NULL );
+    xTaskCreate( vTask_Pot1, "Pot1 Task", 2*configMINIMAL_STACK_SIZE, NULL, 2, &xTask_Pot1);
+    xTaskCreate( vTask_Pot2, "Pot2 Task", 2*configMINIMAL_STACK_SIZE, NULL, 2, &xTask_Pot2);
 
-    //xTaskCreate( vTask_MCU, "LCD Task", 2*configMINIMAL_STACK_SIZE, NULL, 2, NULL );
+
+    xTaskCreate( vTask_BubbleLevel, "Bubble Level Task", 2*configMINIMAL_STACK_SIZE, NULL, 2, &xTask_Bubble );
     /* Start the created tasks running. */
+    xTimerStart(SensorTimer, 0);
     vTaskStartScheduler();
 
     /* Execution will only reach here if there was insufficient heap to
